2674ca2b5eb8aeebe87f6bd696a14d8b27f1316e
grayasgray <alexgray.au@gmail.com>
2026-05-24 10:22:04 +1000

    app improvements
    Manrope font used for info text

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * cache-first approach which caused stale versions to stick around.
  */
 
-const CACHE = 'italia2026-v20';
+const CACHE = 'italia2026-v21';
 
 // Files to pre-cache on install
 const PRECACHE_ASSETS = [
