46ff48a8e853bf69f816075004252095c53283ae
grayasgray <alexgray.au@gmail.com>
2026-05-24 10:36:48 +1000

    app icon update

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * cache-first approach which caused stale versions to stick around.
  */
 
-const CACHE = 'italia2026-v21';
+const CACHE = 'italia2026-v22';
 
 // Files to pre-cache on install
 const PRECACHE_ASSETS = [
