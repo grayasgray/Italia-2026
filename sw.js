05f1ffe3655a8d05c2d8f43866095b48d0d5dcbd
grayasgray <alexgray.au@gmail.com>
2026-05-26 21:00:16 +1000

    app fix
    fonts

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * cache-first approach which caused stale versions to stick around.
  */
 
-const CACHE = 'italia2026-v23';
+const CACHE = 'italia2026-v25';
 
 // Files to pre-cache on install
 const PRECACHE_ASSETS = [
